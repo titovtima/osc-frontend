b5f82f4187a7e9efafb867fe1929e626e71beb7b
titovtima <titov.tima.spb@yandex.ru>
2025-11-01 15:23:07 +0300

    auxes edit page

---
+++ app/pages/auxes.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <ChannelSetup v-for="(_, index) in channels" v-model:channel="channels[index]" 
+      @delete="() => { channels = channels.filter((_, delInd) => delInd != index) }"/>
+    <button @click="addChannel">+</button>
+    <button @click="save">Save</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const channels: Ref<any[]> = ref([]);
+
+fetch(httpHost + '/auxes').then(res => res.json()).then(res => {
+  channels.value = res.auxes;
+});
+
+function addChannel() {
+  let maxNum = 0;
+  for (let channel of channels.value) {
+    maxNum = Math.max(maxNum, channel.number);
+  }
+  channels.value.push({number: maxNum+1, name: 'aux ' + (maxNum+1), color: 'ffffff'})
+}
+
+function save() {
+  if (!check()) {
+    alert('incorrect configuration: 2 channels have the same numbers');
+    return;
+  }
+  channels.value.sort((a, b) => a.number - b.number);
+  fetch(httpHost + '/auxes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({auxes: channels.value})
+  }).then(res => {
+    if (res.ok)
+      alert('saved successfully')
+    else
+      alert('server answered with error');
+  }).catch(() => alert('error saving'));
+}
+
+function check(): boolean {
+  for (let i1 = 0; i1 < channels.value.length; i1++) {
+    for (let i2 = 0; i2 < channels.value.length; i2++) {
+      if (i1 != i2 && channels.value[i1].number == channels.value[i2].number) {
+        return false
+      }
+    }
+  }
+  return true
+}
+</script>

65e1416ce16a7f0b8ac27aa5f52b564008bbdb19
titovtima <titov.tima.spb@yandex.ru>
2025-11-01 21:16:02 +0300

    add pan

--- app/components/ChannelShow.vue
+++ app/components/ChannelShow.vue
@@ -3,21 +3,39 @@
     <div>
       {{ name }}
     </div>
-    <div style="display: flex;">
-      <button style="flex: 0 1 auto;" ref="minusButton">
+    <div style="display: flex; align-items: stretch;">
+      <button style="flex: 0 1 auto;" ref="minusButtonPan" class="unselectable">
         -
       </button>
-      <div style="flex: 1 1 20px; background-color: #aaa; padding: 2px; position: relative; touch-action: none;" ref="bar">
-        <div style="background-color: #fff; height: 100%;" :style="{width: dbToSlider(valueRef) * 100 / maxValue + '%'}">
-
-        </div>
-        <div style="position: absolute; bottom: 0; left: 50%;">
-          <div style="position: relative; left: -50%; height: min-content;">
-            {{ Math.round(valueRef * roundWith) / roundWith }}
+      <div style="flex: 1 1 20px; width: 100%; margin: 0 0.5rem;">
+        <div style="height: 100%; display: flex; align-items: center; position: relative; touch-action: none;" ref="panElem">
+          <div style="width: 100%; height: 0.1rem; background-color: #aaa; z-index: -2;">
+          </div>
+          <div class="unselectable"
+            style="position: absolute; width: 1rem; height: 1rem; border-radius: 0.5rem; background-color: #555; z-index: -1; text-align: center;" 
+            :style="{left: 'calc(' + panToSlider(panRef) + '% - 0.5rem)' }">
+            {{ Math.round(panRef * panRound) / panRound }}
           </div>
         </div>
       </div>
-      <button style="flex: 0 1 auto;" ref="plusButton">
+      <button style="flex: 0 1 auto;" ref="plusButtonPan" class="unselectable">
+        +
+      </button>
+    </div>
+    <div style="display: flex;">
+      <button style="flex: 0 1 auto;" ref="minusButtonLvl" class="unselectable">
+        -
+      </button>
+      <div style="flex: 1 1 20px; background-color: #aaa; padding: 2px; margin: 0 0.5rem; position: relative; touch-action: none;" ref="bar">
+        <div style="background-color: #fff; height: 100%;" :style="{width: dbToSlider(levelRef) * 100 / maxValue + '%'}">
+        </div>
+        <div style="position: absolute; bottom: 0; left: 50%;">
+          <div style="position: relative; left: -50%; height: min-content;" class="unselectable">
+            {{ Math.round(levelRef * roundWith) / roundWith }}
+          </div>
+        </div>
+      </div>
+      <button style="flex: 0 1 auto;" ref="plusButtonLvl" class="unselectable">
         +
       </button>
     </div>
@@ -27,32 +45,41 @@
 <script setup lang="ts">
 let props = defineProps({
   name: String,
-  value: {
+  level: {
     type: Number,
-    required: true
-  }
+    required: true,
+  },
+  pan: {
+    type: Number,
+    required: true,
+  },
 });
 
-let emit = defineEmits(['update:value']);
+let emit = defineEmits(['update:level', 'update:pan']);
 
 let bar = ref();
-let plusButton = ref();
-let minusButton = ref();
+let panElem = ref();
+let plusButtonLvl = ref();
+let minusButtonLvl = ref();
+let plusButtonPan = ref();
+let minusButtonPan = ref();
 
-let valueRef = ref(props.value);
-watch(() => props.value, () => {
-  valueRef.value = props.value;
+let panRef = ref(props.pan);
+watch(() => props.pan, () => {
+  panRef.value = props.pan;
+});
+
+let levelRef = ref(props.level);
+watch(() => props.level, () => {
+  levelRef.value = props.level;
 });
 
 const maxValue = 100;
 const minValue = 0;
 const step = 0.5;
-
 const minDbValue = -150;
 const maxDbValue = 10;
-
 const logBase = 100;
-
 const roundWith = 10;
 
 function sliderToDb(value: number): number {
@@ -63,40 +90,84 @@ function dbToSlider(db: number): number {
 	return (Math.pow(logBase, (db - minDbValue) / (maxDbValue - minDbValue)) - (1 - minValue)) * (maxValue - minValue)/(logBase - 1);
 }
 
+const maxPanValue = 1;
+const minPanValue = -1;
+const panRound = 100;
+const panStep = 0.01;
+
+function sliderToPan(value: number): number {
+  return value / 50 - 1;
+}
+
+function panToSlider(pan: number): number {
+  return (pan + 1) * 50;
+}
+
 onMounted(() => {
-  let handler1 = (x: number, y: number) => {
-    let barRect = bar.value.getBoundingClientRect()
-    let newValue = (x - barRect.x + 2) * maxValue / (barRect.width - 4)
-    // console.log('click', barRect, event, newValue);
+  let handlerLvl = (event: PointerEvent) => {
+    let barRect = bar.value.getBoundingClientRect();
+    let newValue = (event.clientX - barRect.x + 2) * maxValue / (barRect.width - 4);
 
     newValue = Math.max(minValue, Math.min(maxValue, newValue));
 
-    valueRef.value = sliderToDb(newValue);
-    emit('update:value', valueRef.value);
-  }
-
-  let handler = (event: PointerEvent) => {
-    // console.log('pointermove event', event);
-    handler1(event.clientX, event.clientY);
+    levelRef.value = sliderToDb(newValue);
+    emit('update:level', levelRef.value);
   }
 
   bar.value.addEventListener('pointerdown', (event: PointerEvent) => {
-    handler(event);
-    bar.value.addEventListener('pointermove', handler);
+    handlerLvl(event);
+    bar.value.addEventListener('pointermove', handlerLvl);
   });
+
+  plusButtonLvl.value.onclick = () => {
+    levelRef.value = Math.min(maxDbValue, levelRef.value + step);
+    emit('update:level', levelRef.value);
+  };
+
+  minusButtonLvl.value.onclick = () => {
+    levelRef.value = Math.max(minDbValue, levelRef.value - step);
+    emit('update:level', levelRef.value);
+  };
+
+  let handlerPan = (event: PointerEvent) => {
+    let barRect = panElem.value.getBoundingClientRect();
+    let newValue = (event.clientX - barRect.x + 2) * 100 / (barRect.width - 4);
+
+    newValue = Math.max(minValue, Math.min(maxValue, newValue));
+
+    panRef.value = sliderToPan(newValue);
+    emit('update:pan', panRef.value);
+  };
+
+  panElem.value.addEventListener('pointerdown', (event: PointerEvent) => {
+    handlerPan(event);
+    panElem.value.addEventListener('pointermove', handlerPan);
+  });
+
+  plusButtonPan.value.onclick = () => {
+    panRef.value = Math.min(maxPanValue, panRef.value + panStep);
+    emit('update:pan', panRef.value);
+  };
+
+  minusButtonPan.value.onclick = () => {
+    panRef.value = Math.max(minPanValue, panRef.value - panStep);
+    emit('update:pan', panRef.value);
+  };
 
   document.addEventListener('pointerup', () => {
-    bar.value.removeEventListener('pointermove', handler);
+    bar.value.removeEventListener('pointermove', handlerLvl);
+    panElem.value.removeEventListener('pointermove', handlerPan);
   });
-
-  plusButton.value.onclick = () => {
-    valueRef.value = Math.min(maxDbValue, valueRef.value + step);
-    emit('update:value', valueRef.value);
-  };
-
-  minusButton.value.onclick = () => {
-    valueRef.value = Math.max(minDbValue, valueRef.value - step);
-    emit('update:value', valueRef.value);
-  };
 });
 </script>
+
+<style>
+.unselectable {
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+</style>
--- app/pages/aux.vue
+++ app/pages/aux.vue
@@ -6,7 +6,9 @@
       </select>
     </div>
     <ChannelShow v-for="channel in channels" :name="channel.name" 
-      :value="values[currentAuxNum][channel.number]" @update:value="(value) => sendToServer(channel.number, currentAuxNum, value)"/>
+      :level="levels[currentAuxNum][channel.number]" @update:level="(value) => sendLevelToServer(channel.number, currentAuxNum, value)"
+      :pan="pans[currentAuxNum][channel.number]" @update:pan="(value) => sendPanToServer(channel.number, currentAuxNum, value)"
+      />
   </div>
 </template>
 
@@ -20,9 +22,11 @@ const auxes: Ref<Array<any>> = ref([{number: 0, name: "aux 0", color: "ffffff"}]
 const maxAux = 32;
 const maxChannel = 64;
 
-const values: Ref<any> = ref(new Array(maxAux+1));
+const levels: Ref<any> = ref(new Array(maxAux+1));
+const pans: Ref<any> = ref(new Array(maxAux+1));
 for (let i = 0; i <= maxAux; i++) {
-  values.value[i] = new Array<number>(maxChannel+1).fill(0);
+  levels.value[i] = new Array<number>(maxChannel+1).fill(0);
+  pans.value[i] = new Array<number>(maxChannel+1).fill(0);
 }
 
 function changeAux(num: number) {
@@ -47,11 +51,16 @@ function createWs() {
 
   ws.onmessage = (event) => {
     let data = JSON.parse(event.data);
-    let levels: string[] = data.address.split('/');
-    if (levels.length == 6 && levels[1] == 'channel' && levels[3] == 'send' && levels[5] == 'level') {
-      let channelNum = Number(levels[2]);
-      let auxNum = Number(levels[4]);
-      values.value[auxNum][channelNum] = data.args[0];
+    let addrLevels: string[] = data.address.split('/');
+    if (addrLevels.length == 6 && addrLevels[1] == 'channel' && addrLevels[3] == 'send' && addrLevels[5] == 'level') {
+      let channelNum = Number(addrLevels[2]);
+      let auxNum = Number(addrLevels[4]);
+      levels.value[auxNum][channelNum] = data.args[0];
+    }
+    if (addrLevels.length == 6 && addrLevels[1] == 'channel' && addrLevels[3] == 'send' && addrLevels[5] == 'pan') {
+      let channelNum = Number(addrLevels[2]);
+      let auxNum = Number(addrLevels[4]);
+      pans.value[auxNum][channelNum] = data.args[0];
     }
   };
 
@@ -69,7 +78,11 @@ function createWs() {
   }
 }
 
-function sendToServer(channel: number, aux: number, value: number) {
+function sendLevelToServer(channel: number, aux: number, value: number) {
   ws.send(JSON.stringify({address: '/channel/' + channel + '/send/' + aux + '/level', args: [value]}))
+}
+
+function sendPanToServer(channel: number, aux: number, value: number) {
+  ws.send(JSON.stringify({address: '/channel/' + channel + '/send/' + aux + '/pan', args: [value]}))
 }
 </script>

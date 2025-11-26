456a0c45d5a80e90694418be1e1001d15e0aa8f2
titovtima <titov.tima.spb@yandex.ru>
2025-11-26 19:55:26 +0300

    get config from backend

--- app/utils/config.ts
+++ app/utils/config.ts
@@ -1,11 +1,20 @@
+export const config = {
+    // host: '10.1.3.20:8002',
+    host: '127.0.0.1:8002',
+    maxChannel: 80,
+    maxAux: 32,
+    consoleType: 'sd'
+}
+
 export async function getConfig() {
     if (import.meta.server)
-        return {
-            host: import.meta.env.BACKEND_HOST,
-            maxChannel: Number(import.meta.env.MAX_CHANNEL),
-            maxAux: Number(import.meta.env.MAX_AUX),
-            consoleType: import.meta.env.CONSOLE_TYPE,
-        }
+        return await fetch('http://' + config.host + '/config').then(res => res.json()).then(res => {
+            config.host = res.backendHost + ":" + res.httpServerPort;
+            config.maxChannel = res.maxChannel;
+            config.maxAux = res.maxAux;
+            config.consoleType = res.console.series;
+            return config;
+        });
     else
         return await fetch('/config.json').then(res => res.json())
 }
